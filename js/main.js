ca96ab47cb76727bb598668bba15c3a4d87483c6
picoloman6 <a2328b@naver.com>
2024-01-10 12:36:11 +0900

    디렉토리 생성

---
+++ js/main.js
@@ -0,0 +1,64 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const movieCardList = document.getElementById('movieCardList');
+  const movieSearchInput = document.querySelector('.form-control');
+  movieSearchInput.focus();
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization:
+        'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMTBiOWI1OTdkMzIzZjQxZjRhNzE0YmVhYWE1YWM4ZSIsInN1YiI6IjY1OTc3M2IxYTZjMTA0MTBkZGZhYTA1OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Ia1OS9T6UO-9ukTNWALTWszMDW9HDPF_c9PWhNwjz6A'
+    }
+  };
+
+  fetch(
+    'https://api.themoviedb.org/3/movie/top_rated?language=ko-KR&page=1',
+    options
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      data.results.forEach((movie) => {
+        const card = createMovieCard(movie);
+        movieCardList.appendChild(card);
+      });
+    });
+
+  const createMovieCard = (movie) => {
+    const card = document.createElement('div');
+    card.classList.add('col-md-3', 'mb-4');
+    card.innerHTML = `
+                <div class="card" onclick="showMovieId(${movie.id})">
+                    <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" class="card-img-top" alt="${movie.title}">
+                        <div class="card-body">
+                            <h5 class="card-title">${movie.title}</h5>
+                            <p class="card-text">${movie.overview}</p>
+                            <p class="card-text2">Vote Average: ${movie.vote_average}</p>
+                        </div>
+                </div>
+                `;
+    return card;
+  };
+
+  window.showMovieId = function (movieId) {
+    alert(`영화 ID값: ${movieId}`);
+  };
+});
+
+const filterMovies = () => {
+  const searchInput = document.querySelector('.form-control');
+  const searchTerm = searchInput.value.toLowerCase();
+
+  const movieCards = document.querySelectorAll('.col-md-3');
+
+  movieCards.forEach((card) => {
+    const title = card.querySelector('.card-title').textContent.toLowerCase();
+
+    const regex = new RegExp(searchTerm, 'i');
+    if (regex.test(title)) {
+      card.style.display = 'block';
+    } else {
+      card.style.display = 'none';
+    }
+  });
+};
