0a62b2370a98c8357af0a848babdccb7c1e3158b
picoloman6 <a2328b@naver.com>
2024-01-10 16:20:48 +0900

    비동기 함수 수정, createMovieCard 분리, filterMoives 위치 이동

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 // localStorage에 movieId로 영화 댓글 정보 저장
 const setComment = (movieId, name, password) => {
   const exData = localStorage.getItem(movieId);
-  let id = 1;
   let data = [];
+  let id = 1;
 
   if (exData) {
     data = JSON.parse(exData);
@@ -27,53 +27,23 @@ const removeComment = (movieId, commentId) => {
   localStorage.setItem(movieId, JSON.stringify(data));
 };
 
-document.addEventListener('DOMContentLoaded', () => {
-  const movieCardList = document.getElementById('movieCardList');
-  const movieSearchInput = document.querySelector('.form-control');
-  movieSearchInput.focus();
+// 영화 정보 카드 DOM 생성
+const createMovieCard = (movie) => {
+  const card = document.createElement('div');
+  card.classList.add('col-md-3', 'mb-4');
+  card.innerHTML = `
+    <div class="card" onclick="showMovieId(${movie.id})">
+        <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" class="card-img-top" alt="${movie.title}">
+            <div class="card-body">
+                <h5 class="card-title">${movie.title}</h5>
+                <p class="card-text">${movie.overview}</p>
+                <p class="card-text2">Vote Average: ${movie.vote_average}</p>
+            </div>
+    </div>`;
+  return card;
+};
 
-  const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization:
-        'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMTBiOWI1OTdkMzIzZjQxZjRhNzE0YmVhYWE1YWM4ZSIsInN1YiI6IjY1OTc3M2IxYTZjMTA0MTBkZGZhYTA1OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Ia1OS9T6UO-9ukTNWALTWszMDW9HDPF_c9PWhNwjz6A'
-    }
-  };
-
-  fetch(
-    'https://api.themoviedb.org/3/movie/top_rated?language=ko-KR&page=1',
-    options
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      data.results.forEach((movie) => {
-        const card = createMovieCard(movie);
-        movieCardList.appendChild(card);
-      });
-    });
-
-  const createMovieCard = (movie) => {
-    const card = document.createElement('div');
-    card.classList.add('col-md-3', 'mb-4');
-    card.innerHTML = `
-                <div class="card" onclick="showMovieId(${movie.id})">
-                    <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" class="card-img-top" alt="${movie.title}">
-                        <div class="card-body">
-                            <h5 class="card-title">${movie.title}</h5>
-                            <p class="card-text">${movie.overview}</p>
-                            <p class="card-text2">Vote Average: ${movie.vote_average}</p>
-                        </div>
-                </div>
-                `;
-    return card;
-  };
-
-  window.showMovieId = function (movieId) {
-    console.log(getComments(movieId));
-  };
-});
-
+// 영화 이름 검색
 const filterMovies = () => {
   const searchInput = document.querySelector('.form-control');
   const searchTerm = searchInput.value.toLowerCase();
@@ -91,3 +61,32 @@ const filterMovies = () => {
     }
   });
 };
+
+document.addEventListener('DOMContentLoaded', async () => {
+  const movieCardList = document.getElementById('movieCardList');
+  const movieSearchInput = document.querySelector('.form-control');
+  movieSearchInput.focus();
+
+  const url =
+    'https://api.themoviedb.org/3/movie/top_rated?language=ko-KR&page=1';
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization:
+        'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMTBiOWI1OTdkMzIzZjQxZjRhNzE0YmVhYWE1YWM4ZSIsInN1YiI6IjY1OTc3M2IxYTZjMTA0MTBkZGZhYTA1OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Ia1OS9T6UO-9ukTNWALTWszMDW9HDPF_c9PWhNwjz6A'
+    }
+  };
+
+  const res = await fetch(url, options).then((response) => response.json());
+  const data = res.results;
+
+  data.forEach((movie) => {
+    const card = createMovieCard(movie);
+    movieCardList.appendChild(card);
+  });
+
+  window.showMovieId = function (movieId) {
+    console.log(getComments(movieId));
+  };
+});
