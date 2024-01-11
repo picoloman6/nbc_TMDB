0fa3ac9be910487fdfb1203abf97f76dea35e6f7
picoloman6 <a2328b@naver.com>
2024-01-11 16:43:35 +0900

    옵션즈 추가

--- js/detail.js
+++ js/detail.js
@@ -5,6 +5,14 @@ const $commentBox = document.querySelector('.commentBox');
 const $commentContainer = document.querySelector('.commentContainer');
 
 // 변수
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization:
+      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMTBiOWI1OTdkMzIzZjQxZjRhNzE0YmVhYWE1YWM4ZSIsInN1YiI6IjY1OTc3M2IxYTZjMTA0MTBkZGZhYTA1OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Ia1OS9T6UO-9ukTNWALTWszMDW9HDPF_c9PWhNwjz6A'
+  }
+};
 let movieId = 0;
 
 // localStorage에 movieId로 영화 댓글 정보 저장
