8f0f4bc646802261fb1a50f3464ad1ef1d74b15d
picoloman6 <a2328b@naver.com>
2024-01-11 16:11:26 +0900

    댓글 생성 후 렌더링 방식 및 처음 댓글 다는 영화에서 오류 해결

--- js/detail.js
+++ js/detail.js
@@ -13,7 +13,7 @@ const setComment = (movieId, name, password, comment) => {
   let id = 1;
   const exData = localStorage.getItem(movieId);
 
-  if (exData.length > 2) {
+  if (exData && exData.length > 2) {
     data = JSON.parse(exData);
     id = data[data.length - 1].id + 1;
   }
@@ -108,19 +108,24 @@ document.addEventListener('DOMContentLoaded', () => {
   movieId = movie.id;
 
   renderMoiveDetail(movie);
-  comments.forEach((v) => {
-    renderComment(v);
-  });
+
+  if (comments) {
+    comments.forEach((v) => {
+      renderComment(v);
+    });
+  }
 });
 
 // 댓글 입력 기능
-document.querySelector('.commentBtn').addEventListener('click', () => {
+document.querySelector('.commentBtn').addEventListener('click', (e) => {
   const name = $commentName.value;
   const pw = $commentPw.value;
   const comment = $commentBox.value;
 
+  e.preventDefault();
+
   if (name === '' && pw === '' && comment === '') {
-    console.log('값을 입력하세요');
+    alert('값을 입력하세요');
     return;
   }
 
@@ -128,6 +133,14 @@ document.querySelector('.commentBtn').addEventListener('click', () => {
   $commentName.value = '';
   $commentPw.value = '';
   $commentBox.value = '';
+
+  while ($commentContainer.firstChild) {
+    $commentContainer.removeChild($commentContainer.firstChild);
+  }
+
+  getComments(movieId).forEach((v) => {
+    renderComment(v);
+  });
 });
 
 // 댓글 삭제
@@ -140,8 +153,6 @@ $commentContainer.addEventListener('click', (e) => {
       (v) => v.id * 1 === dataset.commentId * 1
     )[0];
     const input = document.querySelector(`#commentDlePW${dataset.commentId}`);
-
-    console.log(comment);
 
     if (input.value !== comment.password) {
       alert('비밀번호가 일치하지 않습니다.');
