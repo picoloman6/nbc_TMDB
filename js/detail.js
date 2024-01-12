83400b375e0e4ab9ae3658c1c425abbfec228a6f
picoloman6 <a2328b@naver.com>
2024-01-12 16:40:52 +0900

    dotenv 적용

--- js/detail.js
+++ js/detail.js
@@ -1,3 +1,7 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 import '../css/detail.css';
 import '../css/reset.css';
 
--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 import '../css/reset.css';
 import '../css/main.css';
 
